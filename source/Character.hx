7a5a8bc659a69ccc65851b7b4a50ca3700e987cd
WizardMantis441 <77557487+WizardMantis441@users.noreply.github.com>
2022-10-10 02:30:33 -0500

    i got todos awesome

--- source/Character.hx
+++ source/Character.hx
@@ -460,16 +460,13 @@ class Character extends FlxSprite
 		}
 
 		dance();
-		if (isPlayer)
-		{
+		if (isPlayer) { // TODO: auto offsets for if the char is both player and opponent
 			flipX = !flipX;
-			if (!curCharacter.startsWith('bf'))
-			{
+			if (!curCharacter.startsWith('bf')) {
 				var oldRight = animation.getByName('singRIGHT').frames;
 				animation.getByName('singRIGHT').frames = animation.getByName('singLEFT').frames;
 				animation.getByName('singLEFT').frames = oldRight;
-				if (animation.getByName('singRIGHTmiss') != null)
-				{
+				if (animation.getByName('singRIGHTmiss') != null) {
 					var oldMiss = animation.getByName('singRIGHTmiss').frames;
 					animation.getByName('singRIGHTmiss').frames = animation.getByName('singLEFTmiss').frames;
 					animation.getByName('singLEFTmiss').frames = oldMiss;
@@ -482,73 +479,44 @@ class Character extends FlxSprite
 		animOffsets[name] = [x, y];
 
 	private var danced:Bool = false;
-
-	public function dance()
-	{
-		if (!debugMode)
-		{
-			switch (curCharacter)
-			{
-				case 'gf', 'gf-christmas', 'gf-car', 'gf-pixel':
-					if (!animation.curAnim.name.startsWith('hair'))
-					{
+	public function dance() {
+		if (!debugMode) {
+			switch (curCharacter) { // TODO: dance steps idea lol!
+				case 'gf', 'gf-christmas', 'gf-car', 'gf-pixel', 'spooky':
+					if (!animation.curAnim.name.startsWith('hair')) { // only effects gfs anyways lmao! 
 						danced = !danced;
-						if (danced)
-							playAnim('danceRight');
-						else
-							playAnim('danceLeft');
+						if (danced) playAnim('danceRight');
+						else playAnim('danceLeft');
 					}
-				case 'spooky':
-					danced = !danced;
-					if (danced)
-						playAnim('danceRight');
-					else
-						playAnim('danceLeft');
-				default:
-					playAnim('idle');
+				default: playAnim('idle');
 			}
 		}
 	}
 
-	public function playAnim(AnimName:String, Force:Bool = false, Reversed:Bool = false, Frame:Int = 0):Void
-	{
+	public function playAnim(AnimName:String, Force:Bool = false, Reversed:Bool = false, Frame:Int = 0):Void {
 		animation.play(AnimName, Force, Reversed, Frame);
-
 		var daOffset = animOffsets.get(AnimName);
-		if (animOffsets.exists(AnimName))
-			offset.set(daOffset[0], daOffset[1]);
-		else
-			offset.set(0, 0);
-
-		if (curCharacter == 'gf')
-		{
-			if (AnimName == 'singUP' || AnimName == 'singDOWN')
-				danced = !danced;
-			if (AnimName == 'singLEFT')
-				danced = true;
-			else if (AnimName == 'singRIGHT')
-				danced = false;
+		if (animOffsets.exists(AnimName)) offset.set(daOffset[0], daOffset[1]);
+		else offset.set(0, 0);
+		if (curCharacter == 'gf') {
+			if (AnimName == 'singUP' || AnimName == 'singDOWN') danced = !danced;
+			if (AnimName == 'singLEFT') danced = true;
+			else if (AnimName == 'singRIGHT') danced = false;
 		}
 	}
 
-	override function update(elapsed:Float)
-	{
-		if (!curCharacter.startsWith('bf'))
-		{
-			if (animation.curAnim.name.startsWith('sing'))
-				holdTimer += elapsed;
+	override function update(elapsed:Float) {
+		if (!curCharacter.startsWith('bf')) {
+			if (animation.curAnim.name.startsWith('sing')) holdTimer += elapsed;
 
 			var dadVar:Float = 4;
-			if (curCharacter == 'dad')
-				dadVar = 6.1;
-			if (holdTimer >= Conductor.stepCrochet * dadVar * 0.001)
-			{
+			if (curCharacter == 'dad') dadVar = 6.1;
+			if (holdTimer >= Conductor.stepCrochet * dadVar * 0.001) {
 				dance();
 				holdTimer = 0;
 			}
 		}
-		if (curCharacter == 'gf' && animation.curAnim.name == 'hairFall' && animation.curAnim.finished)
-			playAnim('danceRight');
+		if (curCharacter == 'gf' && animation.curAnim.name == 'hairFall' && animation.curAnim.finished) playAnim('danceRight');
 		super.update(elapsed);
 	}
 }
